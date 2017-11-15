e5ca9204f4ede1a2726e68bb39cdbab965a9ee66
Thomas <trobertson@oxy.edu>
2017-11-15 12:29:50 -0800

    loses if hits bottom!

--- src/SpaceInvaders.java
+++ src/SpaceInvaders.java
@@ -32,6 +32,7 @@ public class SpaceInvaders extends JPanel implements ActionListener, KeyListener
     private int frame = 0;
 
     private Player me;
+    public int alien_height;
 
     private ArrayList<Alien> joshi;
     private ArrayList<Alien> conner;
@@ -227,8 +228,17 @@ public class SpaceInvaders extends JPanel implements ActionListener, KeyListener
      * @returns  true if the player has lost, false otherwise
      */
     private boolean hasLostGame() {
+        if (joshi != null && !joshi.isEmpty()) {
+            if (joshi.get(0).y + this.alien_height > canvasHeight) {
+                return true;
+            }
+            //when it touches the bottom show lose screen
+            //}
+
+        }
         return false; // FIXME delete this when ready
     }
+
 
     /* Check if the player has won the game
      * 
